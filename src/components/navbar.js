47ff2784d68423879db64347510fb3c2eb5da7f3
Keeseok Hong <keeseok.hong@itential.com>
2018-11-11 04:12:44 -0500

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
+    click(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="navbar-nav">
-            <a className="nav-item nav-link active" href="#">About</a>
-            <a className="nav-item nav-link active" href="#">Profile</a>
-            <a className="nav-item nav-link active" href="#">Reservation</a>
+            <a className="nav-item nav-link active" href="" onClick={this.click}>About</a>
+            <a className="nav-item nav-link active" href="" onClick={this.click}>Profile</a>
+            <a className="nav-item nav-link active" href="" onClick={this.click}>Reservation</a>
             </div>
             </nav>
         );
