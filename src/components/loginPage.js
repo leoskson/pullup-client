d3cf8ecfba6f0317a8207c91ab152aa13e3f01e6
Leo <lson@onetrust.com>
2018-11-27 16:11:16 -0500

    Logo updated with bakgorund color

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -10,6 +10,7 @@ class LoginPage extends Component {
         const className = 'form-group myField';
         return (
             <div className={className}>
+                <style>{'body { background-color: #12A19C; }'}</style>
                 <label>{field.label}</label>
                 <input className='form-control' type={field.type} {...field.input} />
             </div>
@@ -34,19 +35,27 @@ class LoginPage extends Component {
     render() {
         const { handleSubmit } = this.props;
         return (
-            <form className='myLogin' onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-                <Field
-                    type='email'
-                    label='User email'
-                    name='email'
-                    component={this.renderField} />
-                <Field
-                    type='password'
-                    label='Password'
-                    name='password'
-                    component={this.renderField} />
-                <button type='submit' className='btn btn-primary'>Login</button>
-            </form>
+            <div className='loginWrapper'>
+                <p className='logoImage'>
+                    <img src='./images/mini_logo.png' width='200px' heigth='220px'></img>
+                </p>
+                <form className='myLogin' onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
+                    <Field
+                        type='email'
+                        label='User email'
+                        name='email'
+                        component={this.renderField} />
+                    <Field
+                        type='password'
+                        label='Password'
+                        name='password'
+                        component={this.renderField} />
+                    <div className='buttonGroup'>
+                        <button type='submit' className='btn btn-primary' style= {{marginRight: 5}}>Login</button>
+                        <button type='submit' className='btn btn-primary'>Sign Up</button>
+                    </div>
+                </form>
+            </div>
         );
     }
 }
